e9e624adfa06ea655e0917212991c7a1d69b6217
si3792 <simo.t.iliev@industria.tech>
2017-08-31 12:32:16 +0300

    Added eslint configured for react and jsx, and fixed linting in project

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react';
 require('./app.css');
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-          <h2>Welcome to React</h2>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <h2>Welcome to React</h2>
+            </div>
+        );
+    }
 }
 
 export default App;
